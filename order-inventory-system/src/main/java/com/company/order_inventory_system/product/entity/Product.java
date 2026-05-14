3bf497a71eadc0765ae5283a49d3ac65c5ac11e9
Sneha <snehangap@gmail.com>
2026-05-14 21:09:03 +0530

    [Sneha]: Added intial Product entity setup

--- order-inventory-system/src/main/java/com/company/order_inventory_system/product/entity/Product.java
+++ order-inventory-system/src/main/java/com/company/order_inventory_system/product/entity/Product.java
@@ -1,4 +1,10 @@
 package com.company.order_inventory_system.product.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
 public class Product {
+
 }
