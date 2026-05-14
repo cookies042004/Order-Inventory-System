bfdb5c580ac5a287d193adee245d068094dcaada
Kumar-Aditya-Pratap <128625213+Kumar-Aditya-Pratap@users.noreply.github.com>
2026-05-14 16:50:36 +0530

    [Aditya Pratap]Added: Created Entiy class for Order and OrderItem, Created DTO response class and enums package with orderstatus class

---
+++ order-inventory-system/src/main/java/com/company/order_inventory_system/order/dto/OrderResponse.java
@@ -0,0 +1,4 @@
+package com.company.order_inventory_system.order.dto;
+
+public class OrderResponse {
+}
---
+++ order-inventory-system/src/main/java/com/company/order_inventory_system/order/enums/OrderStatus.java
@@ -0,0 +1,4 @@
+package com.company.order_inventory_system.order.enums;
+
+public class OrderStatus {
+}
