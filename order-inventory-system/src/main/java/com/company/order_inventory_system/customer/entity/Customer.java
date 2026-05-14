514c3517208b51e4063f3c2413f0c8a6ada6291b
akspriya200408@gmail.com <akspriya200408@gmail.com>
2026-05-14 21:09:00 +0530

    [Akshaya Priya]:Added:Initial entity setup for customer module

--- order-inventory-system/src/main/java/com/company/order_inventory_system/customer/entity/Customer.java
+++ order-inventory-system/src/main/java/com/company/order_inventory_system/customer/entity/Customer.java
@@ -1,4 +1,10 @@
 package com.company.order_inventory_system.customer.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "customers")
 public class Customer {
+
 }
