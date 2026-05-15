b9dbc128759f62dbfd321e426999215175ea54a1
Kumar-Aditya-Pratap <128625213+Kumar-Aditya-Pratap@users.noreply.github.com>
2026-05-15 16:38:10 +0530

    [Aditya Pratap]Added: created Order DTO

--- order-inventory-system/src/main/java/com/company/order_inventory_system/order/dto/OrderResponse.java
+++ order-inventory-system/src/main/java/com/company/order_inventory_system/order/dto/OrderResponse.java
@@ -1,4 +1,58 @@
 package com.company.order_inventory_system.order.dto;
 
+import com.company.order_inventory_system.order.enums.OrderStatus;
+
+import java.time.LocalDateTime;
+
 public class OrderResponse {
+
+    private Integer orderId;
+
+    private LocalDateTime orderTms;
+
+    private Integer customerId;
+
+    private OrderStatus orderStatus;
+
+    private Integer storeId;
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public LocalDateTime getOrderTms() {
+        return orderTms;
+    }
+
+    public void setOrderTms(LocalDateTime orderTms) {
+        this.orderTms = orderTms;
+    }
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public Integer getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(Integer storeId) {
+        this.storeId = storeId;
+    }
 }
